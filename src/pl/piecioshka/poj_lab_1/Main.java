54d2af0aa42da6f62f84de20dc44a3e0801180ce
Piotr Kowalski <piecioshka@gmail.com>
2014-04-05 23:20:52 +0200

    Supply throwsy list

--- src/pl/piecioshka/poj_lab_1/Main.java
+++ src/pl/piecioshka/poj_lab_1/Main.java
@@ -3,7 +3,7 @@ package pl.piecioshka.poj_lab_1;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
         Registry reg = new Registry();
         System.out.println("Czas: " + Registry.getDateProvider().getDate());
     }
--- src/pl/piecioshka/poj_lab_1/Registry.java
+++ src/pl/piecioshka/poj_lab_1/Registry.java
@@ -3,26 +3,11 @@ package pl.piecioshka.poj_lab_1;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Registry {
-    public static DateProvider getDateProvider() throws IOException {
-        DateProvider provider = null;
-
+    public static DateProvider getDateProvider() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         Properties prop = new Properties();
         prop.load(new FileInputStream("./src/pl/piecioshka/poj_lab_1/prod.properties"));
-
-        try {
-            try {
-                provider = (DateProvider) Class.forName(prop.getProperty("date_provider")).newInstance();
-            } catch (InstantiationException | IllegalAccessException ex) {
-                Logger.getLogger(Registry.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Registry.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return provider;
+        return (DateProvider) Class.forName(prop.getProperty("date_provider")).newInstance();
     }
 }
