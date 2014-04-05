0a8bbbe0ce3d11021c8e1df4e5a6a9c59ec272f6
Piotr Kowalski <piecioshka@gmail.com>
2014-04-05 23:36:57 +0200

    Set clear day

--- src/pl/piecioshka/poj_lab_1/FixedDateProvider.java
+++ src/pl/piecioshka/poj_lab_1/FixedDateProvider.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class FixedDateProvider implements DateProvider {
     @Override
     public Date getDate() {
-        return new Date(2014, 4, 1);
+        return new Date(100, 4, 1, 12, 34, 56); // Dzień: 01/04/2000 12h 34m 56s
     }
 }
